d7e555ed0f02d9d20c9f269b9eaa334bac283b1f
linrehui <987952265@qq.com>
2019-05-07 17:24:37 +0800

    提交到本次仓库

---
+++ 456/src/qwer/asdf.java
@@ -0,0 +1,21 @@
+package qwer;
+
+public class asdf {
+	
+	private int id;
+	private String username;
+	private String password;
+	private int sex;
+	private String love;
+	
+	public asdf(int id, String username, String password, int sex, String love) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.sex = sex;
+		this.love = love;
+	}
+	
+
+}
